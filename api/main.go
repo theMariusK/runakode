88c0dcf3963ffbc90e916d1beea329935ab89024
theMariusK <mariusklumbys@gmail.com>
2025-11-15 21:54:08 +0200

    Initial base

---
+++ api/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"flag"
+	"encoding/json"
+)
+
+type RunRequest struct {
+	Language string `json:"language"`
+	SourceCode string `json:"source_code"`
+}
+
+type Config struct {
+	SupportedLanguages []string `yaml:"supportedLanguages"`
+}
+
+type RunResponse struct {
+	id int `json:"id"`
+	result string `json:"result"`
+	exitCode int `json:"exit_code"`
+}
+
+func api(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Got a %s request from %s\n", r.Method, r.RemoteAddr)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Wrong Method!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var request RunRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Bad Request!", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, request.SourceCode)
+}
+
+func main() {
+	ip := flag.String("server", "127.0.0.1", `
+	"IP address on which the API will be listening, default is 127.0.0.1"`)
+	port := flag.String("port", "8080", `
+	"Port on which the API will be listening, default is 8080"`)
+	flag.Parse()
+	server := fmt.Sprintf("%s:%s", *ip, *port)
+
+	log.Printf("Starting the API server on %s!\n", server)
+	http.HandleFunc("/api", api)
+	http.ListenAndServe(server, nil)
+}
